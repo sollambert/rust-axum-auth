9065beefa79f08daffbaff014816206e0dd32f65
Solomon Lambert <sol.lambert@gmail.com>
2023-12-14 15:39:33 -0600

    migration update for new columns for users

--- migrations/20231214145144_InitialMigration.up.sql
+++ migrations/20231214145144_InitialMigration.up.sql
@@ -1,6 +1,8 @@
 -- Add migration script here
 CREATE TABLE "users" (
     id INTEGER PRIMARY KEY,
+    uuid VARCHAR(36),
     username VARCHAR(24),
-    pass VARCHAR(24)
+    pass VARCHAR(60),
+    email VARCHAR(254)
 );
