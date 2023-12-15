e4fbdceef442cef40ab68917f6a6a55146045c55
Solomon Lambert <sol.lambert@gmail.com>
2023-12-15 09:18:01 -0600

    update user models to reflect database columns, add traits to User model to allow directly pulling from database

--- src/models/user.rs
+++ src/models/user.rs
@@ -1,23 +1,25 @@
 use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Debug, Serialize, sqlx::FromRow)]
 pub struct User {
     pub id: i32,
     pub uuid: String,
     pub username: String,
-    pub password: String,
+    pub pass: String,
     pub email: String
 }
 
 #[derive(Deserialize)]
 pub struct CreateUser {
     pub username: String,
-    pub password: String,
+    pub pass: String,
     pub email: String
 }
 
 #[derive(Deserialize)]
 pub struct LoginUser {
     pub username: String,
-    pub password: String
+    pub pass: String
 }
 
 #[derive(Clone, Debug, Serialize, sqlx::FromRow)]
