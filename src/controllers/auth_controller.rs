c6fdcbee1493fca638a67f7ba20ade926713a45b
Solomon Lambert <sol.lambert@gmail.com>
2023-12-19 14:54:28 -0600

    return jwt reponse instead of responseuser for login route

--- src/controllers/auth_controller.rs
+++ src/controllers/auth_controller.rs
@@ -8,7 +8,7 @@ use axum::{
 use std::env;
 use bcrypt::verify;
 
-use crate::{models::user::{ResponseUser, LoginUser}, strategies::users};
+use crate::{models::user::{ResponseUser, LoginUser}, strategies::{users, authentication::{AuthError, AuthBody, AuthTokenResponse, generate_token_response}}};
 
 // route function to nest endpoints in router
 pub fn routes() -> Router {
@@ -19,19 +19,18 @@ pub fn routes() -> Router {
 
 async fn login_user(
     Json(payload): Json<LoginUser>,
-) -> (StatusCode, Json<ResponseUser>) {
-    // create empty response user to be used if not verified
-    let response_user = ResponseUser {
-        uuid: String::new(),
-        username: String::new(),
-        email: String::new()
-    };
+) -> Result<Json<AuthTokenResponse>, AuthError> {
+    // check if supplied credentials are not empty
+    if payload.username.is_empty() || payload.pass.is_empty() {
+        return Err(AuthError::MissingCredentials)
+    }
     // get user by username from database
     let result = users::get_db_user_by_username(payload.username).await;
     // if can't get user by username, return 400
     if let Err(e) = result {
         println!("{:?}", e);
-        return (StatusCode::BAD_REQUEST, Json(response_user));
+        // return (StatusCode::BAD_REQUEST, Json(response_user));
+        return Err(AuthError::WrongCredentials)
     }
     // print user data
     println!("{:?}", result);
@@ -39,16 +38,19 @@ async fn login_user(
     let user = result.unwrap();
     // verify supplied password is validated
     if verify(payload.pass, &user.pass).unwrap() {
+        // build response user
         let response_user = ResponseUser {
             uuid: user.uuid,
             username: user.username,
             email: user.email
         };
+        // generate jwt token
+        let auth_token_response = generate_token_response(response_user);
         //todo!("IMPLEMENT AUTH COOKIE");
         // send 201 response with JSON response
-        (StatusCode::CREATED, Json(response_user))
+        Ok(Json(auth_token_response))
     } else {
         // send 400 response with JSON response
-        (StatusCode::BAD_REQUEST, Json(response_user))
+        Err(AuthError::WrongCredentials)
     }
 }
