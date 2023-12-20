1209a67d812514f6277bf5c746c2248c828daaf2
Solomon Lambert <sol.lambert@gmail.com>
2023-12-20 16:55:15 -0600

    remove unnecessary logging

--- src/controllers/users_controller.rs
+++ src/controllers/users_controller.rs
@@ -32,11 +32,9 @@ async fn create_user(
             // then return populated ResponseUser with data from table
             let result = users::get_db_user_by_id(id).await;
             // check result for error and return error code if necessary
-            if let Err(e) = result {
-                println!("{:?}", e);
+            if let Err(_) = result {
                 return (StatusCode::INTERNAL_SERVER_ERROR, Json(response_user));
             }
-            println!("{:?}", result);
             let user = result.unwrap();
             // re-create response_user with populated fields
             let response_user = ResponseUser {
@@ -46,9 +44,8 @@ async fn create_user(
             };
             return (StatusCode::CREATED, Json(response_user))
         },
-        Err(e) => {
-            // print error to console and send 400 BAD REQUEST with empty ResponseUser
-            println!("{:?}", e);
+        Err(_) => {
+            // send 400 BAD REQUEST with empty ResponseUser
             return (StatusCode::BAD_REQUEST, Json(response_user))
         }
     }
